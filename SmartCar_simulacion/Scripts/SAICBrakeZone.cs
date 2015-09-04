9bbf54427d56321d53eff702ef7d207bd30da754
Cano19E <canomax019@gmail.com>
2015-09-03 19:06:10 -0500

    Simulacion de auto

---
+++ SmartCar_simulacion/Scripts/SAICBrakeZone.cs
@@ -0,0 +1,16 @@
+﻿//----------------------------------------------
+//                  Smart AI Car
+//
+// Copyright © 2015 BoneCracker Games
+// http://www.bonecrackergames.com
+//
+//----------------------------------------------
+
+using UnityEngine;
+using System.Collections;
+
+public class SAICBrakeZone : MonoBehaviour {
+	
+	public float targetSpeed = 50;
+	
+}
